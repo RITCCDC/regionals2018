f02e199452bc2cf676bbf9d12d83047af4655292
Joe Graham <joe.graham@mail.rit.edu>
2018-08-28 12:46:24 -0400

    Change idempotency to occur in PowerShell script, fix issue #28

--- roles/tintin/files/Add-Users.ps1
+++ roles/tintin/files/Add-Users.ps1
@@ -1,13 +1,14 @@
 $Users = Import-Csv -Path "C:\users.csv"
 
-$Domain = (Get-ADDomain | select -expand dnsroot)
-
 foreach ($User in $Users)
 {
+    if (-not (@(Get-ADUser -Filter { SamAccountName -eq $User.sam }).Count -eq 0)) {
+        Remove-ADUser -Identity $User.sam
+    }
      $DisplayName = $User.username
      $SAM = $User.sam
      $Description = $User.role
      $Password = $User.password
-     $Email = $SAM + "@" + $domainname
-     New-ADUser -Name "$DisplayName" -DisplayName "$DisplayName" -SamAccountName $SAM -Description "$Descrption" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -EmailAddress $Email -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
+     $Email = $SAM + "@team4.wildeagle.local"
+     New-ADUser -Name "$DisplayName" -DisplayName "$DisplayName" -SamAccountName $SAM -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -EmailAddress $Email -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
 }
