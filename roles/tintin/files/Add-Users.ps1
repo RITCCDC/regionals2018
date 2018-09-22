228333b6c21b466e372caf564f2a428cb649cb01
Joe Graham <jmg3484@rit.edu>
2018-09-22 13:29:02 -0400

    use hostname variable to set hostnames, removed tintin from DNS csv since it gets autocreated

--- roles/tintin/files/Add-Users.ps1
+++ roles/tintin/files/Add-Users.ps1
@@ -2,9 +2,13 @@ $Users = Import-Csv -Path "C:\users.csv"
 
 foreach ($User in $Users)
 {
-    if (-not (@(Get-ADUser -Filter { SamAccountName -eq $User.sam }).Count -eq 0)) {
-        Remove-ADUser -Identity $User.sam
-    }
+     $OriginalErrorActionPreference = $ErrorActionPreference
+     $ErrorActionPreference = 'SilentlyContinue'
+     if (-not (@(Get-ADUser -Filter { SamAccountName -eq $User.sam }).Count -eq 0)) {
+         Remove-ADUser -Identity $User.sam
+     }
+     $ErrorActionPreference = $OriginalErrorActionPreference
+     $ErrorActionPreference = 'SilentlyContinue'
      $DisplayName = $User.username
      $SAM = $User.sam
      $Description = $User.role
